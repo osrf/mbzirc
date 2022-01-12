12d72c2eacb981b9ccbcb99287cb6a3bc3f706a7
Arjo Chakravarty <arjo@openrobotics.org>
2022-01-12 15:08:28 +0800

    Add battery test
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

--- mbzirc_ign/test/test_spawn_uav_battery.cc
+++ mbzirc_ign/test/test_spawn_uav_battery.cc
@@ -17,6 +17,7 @@
 
 #include <gtest/gtest.h>
 
+#include <ignition/msgs.hh>
 #include <ignition/gazebo/TestFixture.hh>
 #include <ignition/gazebo/Util.hh>
 #include <ignition/gazebo/World.hh>
@@ -26,6 +27,8 @@
 
 TEST_F(MBZIRCTestFixture, SpawnUAVTest)
 {
+  /// This test checks that the UAV is spawned correctly.
+
   std::vector<std::pair<std::string,std::string>> params{
     {"name", "x3"},
     {"world", "faster_than_realtime"},
@@ -71,4 +74,95 @@ TEST_F(MBZIRCTestFixture, SpawnUAVTest)
   StopLaunchFile(launchHandle);
 
   ASSERT_TRUE(spawnedSuccessfully);
+}
+
+
+TEST_F(MBZIRCTestFixture, TestBatteryDuration)
+{
+  using namespace std::literals::chrono_literals;
+
+  /// This test checks that the UAV battery gets drained in one minute after
+  /// setting the time to one minute.
+  std::vector<std::pair<std::string,std::string>> params{
+    {"name", "x3"},
+    {"world", "faster_than_realtime"},
+    {"model", "x3_c2"},
+    {"type",  "uav"},
+    {"x", "1"},
+    {"y", "2"},
+    {"z", "0.05"},
+    {"flightTime", "1"}
+  };
+
+  ignition::transport::Node node;
+  auto pub = node.Advertise<ignition::msgs::Twist>("/model/x3/cmd_vel");
+
+  bool spawnedSuccessfully = false;
+
+  std::chrono::steady_clock::duration firstSpawned;
+  std::chrono::steady_clock::duration timeNow;
+  std::chrono::steady_clock::duration dischargedAt;
+
+  bool fullyDischarged = false;
+
+  std::function<void(const ignition::msgs::BatteryState&)> batteryCb
+    = [&](const ignition::msgs::BatteryState &_state) -> void
+    {
+      if (_state.percentage() <= 0 && !fullyDischarged)
+      {
+        dischargedAt = timeNow;
+        fullyDischarged = true;
+      }
+    };
+
+  node.Subscribe("/model/x3/battery/linear_battery/state", batteryCb);
+  SetMaxIter(1000);
+
+  LoadWorld("faster_than_realtime.sdf");
+
+  OnPostupdate([&](const ignition::gazebo::UpdateInfo &_info,
+          const ignition::gazebo::EntityComponentManager &_ecm)
+  {
+    auto worldEntity = ignition::gazebo::worldEntity(_ecm);
+    ignition::gazebo::World world(worldEntity);
+
+    auto modelEntity = world.ModelByName(_ecm, "x3");
+    if (modelEntity != ignition::gazebo::kNullEntity)
+    {
+      if (!spawnedSuccessfully)
+      {
+        firstSpawned = _info.simTime;
+        spawnedSuccessfully = true;
+
+        ignition::msgs::Twist twist;
+        ignition::msgs::Vector3d* vec = new ignition::msgs::Vector3d;
+        vec->set_x(0);
+        vec->set_y(0);
+        vec->set_z(1);
+        twist.set_allocated_linear(vec);
+        pub.Publish(twist);
+      }
+      else
+      {
+        timeNow = _info.simTime;
+      }
+    }
+  });
+
+  StartSim();
+  auto launchHandle = LaunchWithParams("spawn.launch.py", params);
+  WaitForMaxIter();
+
+  using namespace std::literals::chrono_literals;
+
+  while(!fullyDischarged && (timeNow - firstSpawned < 4min))
+  {
+    Step(100);
+  }
+  StopLaunchFile(launchHandle);
+
+  ASSERT_TRUE(spawnedSuccessfully);
+  ASSERT_TRUE(fullyDischarged);
+  ASSERT_NEAR(
+    std::chrono::duration<double>(dischargedAt - firstSpawned).count(), 60, 5);
 }
