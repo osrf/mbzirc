4b0259bc935ac602dd118d9f1ce8137d4b6da44f
Ian Chen <ichen@osrfoundation.org>
2022-03-02 14:09:35 -0800

    update sed logic
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>

--- mbzirc_ign/scripts/update_sea_state_params.sh
+++ mbzirc_ign/scripts/update_sea_state_params.sh
@@ -13,13 +13,13 @@ wind_speed=$3
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # update wave gain param
-find $DIR/../models -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<gain>.*<\/gain>/\1<gain>'"$wave_gain"'<\/gain>/g' {} \;
-find $DIR/../worlds -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<gain>.*<\/gain>/\1<gain>'"$wave_gain"'<\/gain>/g' {} \;
+find $DIR/../models -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<gain>.*<\/gain>\(.*<\/wavefield\)/\1<gain>'"$wave_gain"'<\/gain>\2/g' {} \;
+find $DIR/../worlds -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<gain>.*<\/gain>\(.*<\/wavefield\)/\1<gain>'"$wave_gain"'<\/gain>\2/g' {} \;
+
 
 # update wave period param
-find $DIR/../models -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<period>.*<\/period>/\1<period>'"$wave_period"'<\/period>/g' {} \;
-find $DIR/../worlds -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<period>.*<\/period>/\1<period>'"$wave_period"'<\/period>/g' {} \;
-
+find $DIR/../models -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<period>.*<\/period>\(.*<\/wavefield\)/\1<period>'"$wave_period"'<\/period>\2/g' {} \;
+find $DIR/../worlds -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<period>.*<\/period>\(.*<\/wavefield\)/\1<period>'"$wave_period"'<\/period>\2/g' {} \;
 
 # # update wind linear vel param
-find $DIR/../worlds -type f -exec sed -i ':a;N;$!ba;s/\(<wind>.*\)<linear_velocity>.*<\/linear_velocity>/\1<linear_velocity>'"$wind_speed"'<\/linear_velocity>/g' {} \;
+find $DIR/../worlds -type f -exec sed -i ':a;N;$!ba;s/\(<wind>.*\)<linear_velocity>.*<\/linear_velocity>\(.*<\/wind\)/\1<linear_velocity>'"$wave_linear_velocity"'<\/linear_velocity>\2/g' {} \;
