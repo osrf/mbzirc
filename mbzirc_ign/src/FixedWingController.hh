e46a4aa22e008f4b3e9952106d979644af9845ad
Arjo Chakravarty <arjo@openrobotics.org>
2022-03-15 10:09:30 +0800

    Fix style issues in fixed wing plugin
    Apparantly a clown who goes by the name of @arjo129 does not know how to indent things properly and add license headings.
    
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

--- mbzirc_ign/src/FixedWingController.hh
+++ mbzirc_ign/src/FixedWingController.hh
@@ -1,5 +1,21 @@
-#ifndef MBZIRC_IGN_GAMELOGICPLUGIN_HH_
-#define MBZIRC_IGN_GAMELOGICPLUGIN_HH_
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef MBZIRC_IGN_FIXEDWING_HH_
+#define MBZIRC_IGN_FIXEDWING_HH_
 
 #include <memory>
 #include <ignition/gazebo/System.hh>
@@ -32,7 +48,7 @@ namespace mbzirc
   ///    right flap joint position (string)
   /// `thruster`: Topic to publish to on ignition to control the
   ///    thruster joint velocity (string)
-    class FixedWingControllerPlugin:
+  class FixedWingControllerPlugin:
     public ignition::gazebo::System,
     public ignition::gazebo::ISystemConfigure,
     public ignition::gazebo::ISystemPreUpdate
