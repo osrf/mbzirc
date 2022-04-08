9d2dcd79e937ee8e7436aaff3256cbe653c44111
Arjo Chakravarty <arjo@openrobotics.org>
2022-04-08 08:56:19 +0800

    Reset PIDs
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

--- mbzirc_ign/src/FixedWingController.cc
+++ mbzirc_ign/src/FixedWingController.cc
@@ -50,8 +50,10 @@ class mbzirc::FixedWingControllerPrivate
   public: enum class Mode
   {
     /// \brief Take off mode
-    TAKE_OFF,
-    /// \brief Auto mode
+    TAKE_OFF_START,
+    /// \brief Enter into a climb
+    TAKE_OFF_CLIMB,
+    /// \brief Just use to maintain attitude
     ATTITUDE_CONTROL
   };
 
@@ -132,6 +134,8 @@ class mbzirc::FixedWingControllerPrivate
       &FixedWingControllerPrivate::TakeOffService, this);
   }
 
+
+
   /// \brief Callback for attitude target messages
   /// \param[in] _msg Attitude target message
   public: void OnAttitudeTarget(const msgs::Float_V& _msg)
@@ -151,6 +155,9 @@ class mbzirc::FixedWingControllerPrivate
     targetRoll = euler.X();
     targetPitch = euler.Y();
     targetVelocity = _msg.data(4);
+
+    this->pitchControl.Reset();
+    this->rollControl.Reset();
   }
 
   /// \brief Take off service
@@ -170,8 +177,11 @@ class mbzirc::FixedWingControllerPrivate
     }
     this->takeOffMinPitch = _takeoffParams.data(0);
     this->takeOffAltitude = _takeoffParams.data(1);
-    this->mode = Mode::TAKE_OFF;
+    this->mode = Mode::TAKE_OFF_START;
     _result.set_data(true);
+
+    this->pitchControl.Reset();
+    this->rollControl.Reset();
     return true;
   }
 
@@ -353,7 +363,8 @@ void FixedWingControllerPlugin::PreUpdate(
 
   auto cmd = 0.0;
 
-  if (this->dataPtr->mode == FixedWingControllerPrivate::Mode::TAKE_OFF) {
+  /// State machine for flight controller
+  if (this->dataPtr->mode == FixedWingControllerPrivate::Mode::TAKE_OFF_START) {
     /// Goal of takeoff controller is to increase altitude as much as possible.
     /// Once a basic airspeed is achieved we adjust the angle of attack.
     /// Fix Thruster power-
@@ -361,37 +372,48 @@ void FixedWingControllerPlugin::PreUpdate(
     thrusterPower.set_data(this->dataPtr->takeOffPower);
     this->dataPtr->thruster.Publish(thrusterPower);
 
+    /// Keep the Aircraft on the runway till ready to take off
+    auto pitchError = rotation.Dot(this->dataPtr->pitchAxis);
+    auto offset = this->dataPtr->pitchControl.Update(pitchError, _info.dt);
+
+    /// Control aerelions
+    msgs::Double leftFlap;
+    leftFlap.set_data(offset);
+    this->dataPtr->leftFlap.Publish(leftFlap);
+
+    msgs::Double rightFlap;
+    rightFlap.set_data(offset);
+    this->dataPtr->rightFlap.Publish(rightFlap);
+    cmd = offset;
+
     /// Detect if sufficient lift is achieved
     if (linVel > this->dataPtr->takeOffSpeed) {
+      /// Transition into climb
+      this->dataPtr->mode = FixedWingControllerPrivate::Mode::TAKE_OFF_CLIMB;
       this->dataPtr->targetVelocity = this->dataPtr->takeOffPower;
       this->dataPtr->targetPitch = std::max(0.1, this->dataPtr->takeOffMinPitch);
-      this->dataPtr->ExecuteAttitudeControlLoop(rotation, _info);
-
-      /// Once target altitude is reached level out.
-      if (pose->Data().Z() >= this->dataPtr->takeOffAltitude) {
-        this->dataPtr->mode = FixedWingControllerPrivate::Mode::ATTITUDE_CONTROL;
-        this->dataPtr->targetPitch = 0;
-      }
-    }
-    else
-    {
-      /// Keep the Aircraft on the runway till ready to take off
-      auto pitchError = rotation.Dot(this->dataPtr->pitchAxis);
-      auto offset = this->dataPtr->pitchControl.Update(pitchError, _info.dt);
-
-      /// Control aerelions
-      msgs::Double leftFlap;
-      leftFlap.set_data(offset);
-      this->dataPtr->leftFlap.Publish(leftFlap);
-
-      msgs::Double rightFlap;
-      rightFlap.set_data(offset);
-      this->dataPtr->rightFlap.Publish(rightFlap);
-      cmd = offset;
-
+      this->dataPtr->rollControl.Reset();
+      this->dataPtr->pitchControl.Reset();
     }
   }
-  else if (this->dataPtr->mode == FixedWingControllerPrivate::Mode::ATTITUDE_CONTROL){
+  else if (
+    this->dataPtr->mode == FixedWingControllerPrivate::Mode::TAKE_OFF_CLIMB)
+  {
+    /// Maintain a fixed climb rate
+    this->dataPtr->targetVelocity = this->dataPtr->takeOffPower;
+    this->dataPtr->targetPitch = std::max(0.1, this->dataPtr->takeOffMinPitch);
+    this->dataPtr->ExecuteAttitudeControlLoop(rotation, _info);
+    /// Once target altitude is reached level out.
+    if (pose->Data().Z() >= this->dataPtr->takeOffAltitude) {
+      this->dataPtr->mode = FixedWingControllerPrivate::Mode::ATTITUDE_CONTROL;
+      this->dataPtr->targetPitch = 0;
+      this->dataPtr->rollControl.Reset();
+      this->dataPtr->pitchControl.Reset();
+    }
+  }
+  else if (
+    this->dataPtr->mode == FixedWingControllerPrivate::Mode::ATTITUDE_CONTROL)
+  {
     this->dataPtr->ExecuteAttitudeControlLoop(rotation, _info);
   }
 
