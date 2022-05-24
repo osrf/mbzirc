88efc7946d280c6eb6e33d2b6d2196ea9551dc51
Ian Chen <ichen@osrfoundation.org>
2022-05-24 10:15:53 -0700

    add entry point to docker file
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>

--- docker/mbzirc_sim/run_sim.bash
+++ docker/mbzirc_sim/run_sim.bash
@@ -5,5 +5,10 @@
 
 mkdir -p /tmp/ign/logs
 
+# atop process monitoring
+atop -R -w /tmp/ign/logs/atop_log &
+
 export ROS_LOG_DIR=/tmp/ign/logs/ros
-unbuffer ign gazebo -v 4 "$@" 2>&1 | tee /tmp/ign/logs/gzserver_stdout.log
+# todo(iche033) update this to use a new launch file that laucnhes sim and
+# spawns all robots using spawn_config
+unbuffer ros2 launch ros_ign_gazebo ign_gazebo.launch.py ign_args:="-v 4 -r $@" 2>&1 | tee /tmp/ign/logs/gzserver_stdout.log
