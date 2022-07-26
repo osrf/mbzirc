34f155b4a96f7c0ef0e7aa9e40a37eede1b38783
Alejo Carballude <alejocarballude@gmail.com>
2022-07-26 18:48:10 -0400

    Add stall option to cloudsim_bridge docker entrypoint (#192)
    * Add stall option to cloudsim_bridge docker entrypoint.
    
    * Improve cloudsim_bridge entrypoint documentation.

--- docker/cloudsim_bridge/run_bridge.bash
+++ docker/cloudsim_bridge/run_bridge.bash
@@ -5,6 +5,21 @@
 
 mkdir -p /tmp/ign/logs
 
+# Optionally stall this bridge from starting to allow configuration files to be populated.
+# MBZIRC_STALL_START_PATH contains a path to a file.
+# If MBZIRC_STALL_START_PATH is set, stall the bridge from starting until the file exists.
+if [ -n "${MBZIRC_STALL_START_PATH}" ]; then
+  echo "Waiting for configuration files to be ready."
+  echo "Stalling until ${MBZIRC_STALL_START_PATH} exists."
+
+  until [ -f "${MBZIRC_STALL_START_PATH}" ]; do
+     sleep 1
+  done
+
+  echo "Configurations file ready."
+  echo "Proceeding to start simulation."
+fi
+
 # atop process monitoring
 atop -R -w /tmp/ign/logs/atop_log &
 
